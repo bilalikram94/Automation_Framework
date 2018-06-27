17ce3d0b3005d595c41f1b312c0df3003bd33977
bilalikram94 <bilalikram94@gmail.com>
2018-06-27 16:37:41 +0500

    commit

---
+++ pages/attendence/logs.py
@@ -0,0 +1,59 @@
+from base.basepage import BasePage
+import utilities.custom_logger as cl
+import logging
+import time
+
+
+class Logs(BasePage):
+    log = cl.customLogger(logging.DEBUG)
+    # Locators
+    _side_menu = "//ul[@id='cd-primary-nav']/li[1]"  # By Xpath
+    _attendence = "Attendance"  # By Link
+    _logs = "Logs"  # By Link
+    _details = "Detail"  # By Link
+    _absentees = "Absentees"  # By Link
+    _more_options = "[data-toggle='collapse']"  # By CSS
+    _add_new = ".btn-blue.btn-action"  # By CSS
+    _search_bar = "#basic_search [type]"  # By CSS
+    _table_elements = "tbody [role='row']:nth-of-type(1) .sorting_1"  # By CSS
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    #def clickSideMenu(self):
+    #    self.elementClick(self._side_menu, locatorType='xpath')
+    #    self.util.sleep(3)
+
+    #def clickAttendence(self):
+    #    self.elementClick(self._attendence, locatorType='link')
+    #    time.sleep(2)
+
+    def verifyLogs(self):
+        result = self.isElementPresent(self._logs, locatorType='link')
+        return result
+
+    def verifyDetails(self):
+        result = self.isElementPresent(self._details, locatorType='link')
+        return result
+
+    def verifyAbsentees(self):
+        result = self.isElementPresent(self._absentees, locatorType='link')
+        return result
+
+    def verifyMoreOptions(self):
+        result = self.isElementPresent(self._more_options, locatorType='css')
+        return result
+
+    def verifyAddBtn(self):
+        result = self.isElementPresent(self._add_new, locatorType='css')
+        return result
+
+    def verifySearchBar(self):
+        result = self.isElementPresent(self._search_bar, locatorType='css')
+        return result
+
+    def verifyTable(self):
+        time.sleep(3)
+        result = self.isElementPresent(self._table_elements, locatorType='css')
+        return result
---
+++ tests/attendence/logs.py
@@ -0,0 +1,32 @@
+import pytest
+import unittest
+from utilities.teststatus import Status
+from pages.attendence.logs import Logs
+from pages.home.navigation import Navigation
+
+
+@pytest.mark.usefixtures("oneTimeSetUp", "setUp")
+class TestSideMenu(unittest.TestCase):
+    @pytest.fixture(autouse=True)
+    def classSetup(self, oneTimeSetUp):
+        self.lg = Logs(self.driver)
+        self.ts = Status(self.driver)
+        self.nav = Navigation(self.driver)
+
+    @pytest.mark.run(order=1)
+    def test_Logs(self):
+        self.nav.Attendence()
+        result = self.lg.verifyAbsentees()
+        self.ts.mark(result, "VerifyAbsentees")
+        result1 = self.lg.verifyLogs()
+        self.ts.mark(result1, "Verify Logs")
+        result2 = self.lg.verifyDetails()
+        self.ts.mark(result2, "Verify Details")
+        result3 = self.lg.verifyMoreOptions()
+        self.ts.mark(result3, "Verify More Options")
+        result4 = self.lg.verifyAddBtn()
+        self.ts.mark(result4, "Verify Add Button")
+        result5 = self.lg.verifySearchBar()
+        self.ts.mark(result5, "Verify Search Bar")
+        result6 = self.lg.verifyTable()
+        self.ts.markFinal("Test_Logs", result6, "Verify Table")
